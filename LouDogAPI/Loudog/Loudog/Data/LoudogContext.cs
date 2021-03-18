93f18a90ca145c5fc946ef5c95c47a20ecfa4762
abitj <https://github.com/abitj>
2021-03-17 22:22:24 -0500

    model and db context

---
+++ LouDogAPI/Loudog/Loudog/Data/LoudogContext.cs
@@ -0,0 +1,21 @@
+﻿using Loudog.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loudog.Data
+{
+    public class LoudogContext:DbContext
+    {
+        public LoudogContext(DbContextOptions<LoudogContext> options) : base(options)
+        {
+        }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Sidebar> Sidebar { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+    
+}
---
+++ LouDogAPI/Loudog/Loudog/Model/Page.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loudog.Model
+{
+    public class Page
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Content { get; set; }
+        public string HasSideBar { get; set; }
+    }
+}
